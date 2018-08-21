2208b37a716285e60fdcd017bbc1fcc8fd619988
Max <chretien.max@gmail.com>
2018-08-21 13:37:39 +0200

    [#4] going back to require for server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import { join } from 'path';
-import express, { static } from 'express';
+const path = require('path');
+const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(join(__dirname + '/dist/frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 
 // default Heroku port
